bf8f76f17c64eff2d6ca6029ee0ab7a466590128
Wilfred Hughes <me@wilfred.me.uk>
2018-10-24 00:04:21 +0100

    Tweak indentation

--- racer.el
+++ racer.el
@@ -642,18 +642,20 @@ Commands:
 
 (defun racer-complete--extract-args (str)
   "Extract function arguments from STR (excluding a possible self argument)."
-  (let* ((index (string-match (rx
-			       (or (seq "("
-					(zero-or-more (not (any ",")))
-					"self)")
-				   (seq "("
-					(zero-or-more (seq (zero-or-more (not (any "(")))
-					"self"
-					(zero-or-more space)
-					","))
-					(zero-or-more space)
-					(group (zero-or-more (not (any ")"))))
-					")"))) str))
+  (let* ((index (string-match
+                 (rx
+		  (or (seq "("
+			   (zero-or-more (not (any ",")))
+			   "self)")
+		      (seq "("
+			   (zero-or-more (seq (zero-or-more (not (any "(")))
+					      "self"
+					      (zero-or-more space)
+					      ","))
+			   (zero-or-more space)
+			   (group (zero-or-more (not (any ")"))))
+			   ")")))
+                 str))
 	 (extract (match-string 1 str)))
     (if extract
 	(format "(%s)" extract)
