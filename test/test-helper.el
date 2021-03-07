89fde7e17b2f0be228b14ceb64d262906a043e03
Wilfred Hughes <me@wilfred.me.uk>
2021-03-06 18:21:22 -0800

    Set load-path to helper coverage data collection

--- test/test-helper.el
+++ test/test-helper.el
@@ -4,9 +4,13 @@
 
 ;;; Code:
 
+(require 'ert)
+(require 'f)
+
+(let ((racer-dir (f-parent (f-dirname (f-this-file)))))
+  (add-to-list 'load-path racer-dir))
+
 (require 'undercover)
-(undercover "racer.el"
-            (:exclude "*-test.el")
-            (:report-file "/tmp/undercover-report.json"))
+(undercover "racer.el" (:exclude "*-test.el"))
 
 ;;; test-helper.el ends here
